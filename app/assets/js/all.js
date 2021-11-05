ca61ef7d9600e7b1fd9a4e3289a140c35d3aebe3
meiman <qaz814009@gmail.com>
2021-11-05 23:17:42 +0800

    swiper commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,10 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      768:{
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    }
 });
