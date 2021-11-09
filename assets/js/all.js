7a0f8522528c5f7e3d25fe80a34372a3a906a382
meiman <qaz814009@gmail.com>
2021-11-09 22:14:56 +0800

    Update 2021-11-09T14:14:18.368Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,13 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
